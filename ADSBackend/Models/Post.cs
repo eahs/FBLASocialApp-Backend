283970fdfc310b5acf510c4a0d3491b9c6db1184
Vincent Lauro <vinniehat@gmail.com>
2020-10-21 22:26:42 -0400

    Added the Post Model

---
+++ ADSBackend/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+	public class Post
+	{
+		[Key]
+		public int PostId { get; set; }
+		public int AuthorId { get; set; }
+		public Member Author { get; set; }
+		public string Title { get; set; }
+		public string Body { get; set; }
+		public string Image { get; set; }
+		public bool isMachinePost { get; set; } = false;
+		public DateTime CreatedAt { get; set; }
+		public DateTime EditedAt { get; set; }
+		public bool isDeleted { get; set; } = false;
+		public List<PostReaction> Reactions { get; set; }
+		public List<PostComment> Comments { get; set; }
+
+	}
+}
