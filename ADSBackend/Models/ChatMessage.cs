ee22ad0bd3424b4c671420b4ff95af1cd2a0f23e
Vincent Lauro <vinniehat@gmail.com>
2020-10-21 22:39:30 -0400

    Added the ChatMessage Model

---
+++ ADSBackend/Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+	public class ChatMessage
+	{
+		public int MessageId { get; set; }
+		public Member Author { get; set; }
+		public Member To { get; set; }
+		public string Body { get; set; }
+		public string Image { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime EditedAt { get; set; }
+		public bool isDeleted { get; set; } = false;
+	}
+}
