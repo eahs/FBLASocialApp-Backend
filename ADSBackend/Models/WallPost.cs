c8a1026f05f8447c96544a7f3fc51e7991354241
Vincent Lauro <vinniehat@gmail.com>
2020-10-21 22:30:07 -0400

    Added the WallPost Model

---
+++ ADSBackend/Models/WallPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+	public class WallPost
+	{
+		public int PostId { get; set; }
+		public Post Post { get; set; }
+		public int WallId { get; set; }
+		public Wall Wall { get; set; }
+	}
+}
