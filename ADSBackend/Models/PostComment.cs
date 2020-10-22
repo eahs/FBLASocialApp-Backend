129f4a9626b492b1d4c4b3549983a596c3283e62
Vincent Lauro <vinniehat@gmail.com>
2020-10-21 22:28:21 -0400

    Added the PostComment Model

---
+++ ADSBackend/Models/PostComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+	public class PostComment
+	{
+		public int PostId { get; set; }
+		public Post Post { get; set; }
+		public int CommentId { get; set; }
+		public Comment Comment { get; set; }
+	}
+}
