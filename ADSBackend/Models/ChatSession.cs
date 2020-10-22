7070159d592947bd0fd602ff46dd002d0f265ca7
Vincent Lauro <vinniehat@gmail.com>
2020-10-21 22:35:38 -0400

    Added the ChatSession Model

---
+++ ADSBackend/Models/ChatSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+	public class ChatSession
+	{
+		public int SessionId { get; set; }
+		public Session Session { get; set; }
+		public Member Member1 { get; set; }
+		public Member Member2 { get; set; }
+		List<ChatMessage> Messages { get; set; }
+		public DateTime LastMessageSentAt { get; set; }
+	}
+}
