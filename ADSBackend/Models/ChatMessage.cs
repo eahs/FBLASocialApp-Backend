83df388d9f568ac2003ddc37b36f2164bc443441
Vincent Lauro <vinniehat@gmail.com>
2020-10-23 15:50:36 -0400

    Added Author and To Id to The ChatMessage Model

--- ADSBackend/Models/ChatMessage.cs
+++ ADSBackend/Models/ChatMessage.cs
@@ -8,7 +8,9 @@ namespace ADSBackend.Models
 	public class ChatMessage
 	{
 		public int MessageId { get; set; }
+		public int AuthorId { get; set; }
 		public Member Author { get; set; }
+		public int ToId { get; set; }
 		public Member To { get; set; }
 		public string Body { get; set; }
 		public string Image { get; set; }
