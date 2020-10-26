dc0cecc9e3e81f3b6c8f1b90825b8467818096f0
Vincent Lauro <vinniehat@gmail.com>
2020-10-26 14:16:01 -0400

    Enumerated Reaction Types

--- ADSBackend/Models/Reaction.cs
+++ ADSBackend/Models/Reaction.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace ADSBackend.Models
 {
+	public enum ReactionTypes
+    {
+		Like,
+		Love,
+		Surprised,
+		Curious
+    }
+
 	public class Reaction
 	{
 		[Key]
 		public int ReactionId { get; set; }
-		public int ReactionType { get; set; }
+		public ReactionTypes ReactionType { get; set; } = ReactionTypes.Like;
 		public int MemberId { get; set; }
 		public Member Member { get; set; }
 	}
