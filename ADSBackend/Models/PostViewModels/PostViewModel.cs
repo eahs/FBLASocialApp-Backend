e7924717954a3d032ac26d544a0f812f44e80507
Vincent Lauro <vinniehat@gmail.com>
2020-11-01 13:56:38 -0500

    Created the PostViewModel

---
+++ ADSBackend/Models/PostViewModels/PostViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+
+    public class PostViewModel
+    {
+        [Required]
+        public int PostId { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Image { get; set; }
+        public bool IsMachinePost { get; set; } = false;
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public PrivacyLevel PrivacyLevel { get; set; } = PrivacyLevel.Public;
+        public bool IsFeatured { get; set; }
+
+    }
+}
