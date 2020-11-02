916b9c2710aef1d142bdf43be725678cab7753bf
Vincent Lauro <vinniehat@gmail.com>
2020-11-01 19:22:14 -0500

    Required more Post Fields

--- ADSBackend/Models/Post.cs
+++ ADSBackend/Models/Post.cs
@@ -18,19 +18,26 @@ namespace ADSBackend.Models
 	{
 		[Key]
 		public int PostId { get; set; }
+		[Required]
 		public int AuthorId { get; set; }
 		public Member Author { get; set; }
+		[Required]
 		public string Title { get; set; }
 		public string Body { get; set; }
 		public string Image { get; set; }
+		[Required]
 		public bool IsMachinePost { get; set; } = false;
+		[Required]
 		public DateTime CreatedAt { get; set; }
 		public DateTime EditedAt { get; set; }
+		[Required]
 		public bool IsDeleted { get; set; } = false;
 		public List<PostReaction> Reactions { get; set; }
 		public List<PostComment> Comments { get; set; }
+		[Required]
 		public PrivacyLevel PrivacyLevel { get; set; } = PrivacyLevel.Public;
 		public int FavoriteCount { get; set; }
+		[Required]
 		public bool IsFeatured { get; set; } = false;
 		[NotMapped] public bool IsFavorite { get; set; } = false;
 
