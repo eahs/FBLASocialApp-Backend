1eced772a42c7272eda8e3f12918cd76e3527068
Vincent Lauro <vinniehat@gmail.com>
2020-11-11 10:42:58 -0500

    Added automatic migrations

--- ADSBackend/Startup.cs
+++ ADSBackend/Startup.cs
@@ -158,6 +158,8 @@ namespace ADSBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -209,6 +211,19 @@ namespace ADSBackend
                 // seed the AspNetUsers table
                 var userSeed = new ApplicationUserSeed(userManager);
                 userSeed.CreateAdminUser();
+            }
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>())
+                {                    
+                    context.Database.Migrate();
+                }
             }
         }
     }
